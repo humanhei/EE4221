72de234de259363ec32b68d278ab9aec855dd60d
kincchung <kincchung@gmail.com>
2021-10-26 22:55:27 +0800

    query update

--- PHP Website/index.php
+++ PHP Website/index.php
@@ -1,11 +1,69 @@
 <html>
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Example Site;!</title>
-	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	<link rel="stylesheet" href="./css/styles.css">
+
 </head>
 
 <body class="bodyStyle">
+
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+   
+   <a class="navbar-brand" href="index.php">
+	 <img src="Logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+	 <span class="h3 mx-1">Home</span>
+   </a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+	 <span class="navbar-toggler-icon"></span>
+   </button>
+   
+  
+
+   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	 <ul class="navbar-nav mr-auto">
+
+	   <li class="nav-item active">
+		 <a class="nav-link" href="#aboutUs">About Us</a>
+	   </li>
+	   <li class="nav-item">
+		 <a class="nav-link" href="#contactUs">Contact Us</a>
+	   </li>
+
+	   <li class="nav-item">
+		 <a class="nav-link" href="query.php">Query</a>
+	   </li>
+
+
+
+
+
+	   <!-- <li class="nav-item dropdown">
+		 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Query</a>
+
+		 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+		   <a class="dropdown-item" href="#">網頁設計</a>
+		   <a class="dropdown-item" href="#">平面設計</a>
+		 </div>
+	   </li> -->
+	 </ul>
+	 <form class="form-inline my-2 my-lg-0">
+	   <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+	   <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search
+	   </button>
+	 </form>
+   </div>
+   
+ </nav>
+
+
+
+
+
 
 	<div id="header" class="mainHeader">
 		<hr>
@@ -52,7 +110,7 @@
 		</div>
 			<table>
 				<tr>
-					<td><figure><img src="Shirley.jpeg" height=auto width="400"><figcaption>Shirley Rodriguez</figcaption></figure></td>
+					<td><figure><img src="Shirley.png" height=auto width="400"><figcaption>Shirley Rodriguez</figcaption></figure></td>
 				</tr>	
 					<tr><td><p>Our site got started when Shirley Rodriguez found that she was frequently looking up data from a variety of databases. <br>Shirley decided to start sharing some of this data with other social researchers. </p></td>
 				</tr>
@@ -82,3 +140,9 @@
 	</div>
 </body>
 </html>
+<!-- jQuery CDN - Slim version (=without AJAX) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
